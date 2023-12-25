381c96262cf402116209ae9a623fb635dcd7dd64
taku10101 <taku1011ht@gmail.com>
2023-12-25 09:38:14 +0900

    section 12 13 done

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  //   トグルボタンがオンのとき「背景色が変わります。」というテキストの背景色が赤色に変化する。
+  // トグルボタンがオフのとき「背景色が変わります。」というテキストの背景色が無色に変化する。
+  if (e.target.checked) {
+    document.getElementById("text").style.backgroundColor = "red";
+  } else {
+    document.getElementById("text").style.backgroundColor = "";
+  }
 }
